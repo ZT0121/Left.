34b3ef562f805c50db6d8be0fba916478a1d9065
ZTing <zting@napu.edu.pl>
2026-08-17 16:05:29 +0800

    Bump app cache version to 20260817-03

--- js/app.js
+++ js/app.js
@@ -335,7 +335,7 @@
   function registerServiceWorker() {
     if (!("serviceWorker" in navigator)) return;
     window.addEventListener("load", () => {
-      navigator.serviceWorker.register("./sw.js?v=20260817-02")
+      navigator.serviceWorker.register("./sw.js?v=20260817-03")
         .then((registration) => {
           registration.update()
             .catch((error) => console.warn("Service worker update check failed", error));
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION = "left-20260817-02";
-const APP_VERSION = "20260817-02";
+const CACHE_VERSION = "left-20260817-03";
+const APP_VERSION = "20260817-03";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(self.skipWaiting());
